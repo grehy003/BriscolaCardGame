019a99239786ce25a672f8752ef1d882c9f5ec84
grehy003 <grehy003@mymail.unisa.edu.au>
2021-02-09 15:13:25 +1030

    Player function declarations

--- player.h
+++ player.h
@@ -1,11 +1,18 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 
+#include "vector"
+#include "card.h"
 
 class Player
 {
 public:
     Player();
+    Card playCard();
+    void pickUpCard(Card card);
+    bool hasFullHand();
+private:
+    std::vector<Card> _hand;
 };
 
 #endif // PLAYER_H
