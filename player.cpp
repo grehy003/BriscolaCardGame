179fa06c3231134421a661bff68cdfb63647c834
grehy003 <grehy003@mymail.unisa.edu.au>
2021-02-16 12:55:27 +1030

    Player class functions defined

--- player.cpp
+++ player.cpp
@@ -4,3 +4,28 @@ Player::Player()
 {
 
 }
+
+Card Player::playCard()
+{
+    return Card(0,(Suit)0);
+}
+
+void Player::pickUpCard(Card card)
+{
+    _hand.push_back(card);
+}
+
+bool Player::hasFullHand()
+{
+    return _hand.size() == 3;
+}
+
+void Player::addScore(int score)
+{
+    _points += score;
+}
+
+int Player::points()
+{
+    return _points;
+}
